8b9f02783b88277388b33c244b0935fcaf663e13
ChicheT <tsai122@purdue.edu>
2021-08-24 21:32:40 -0400

    web rwd layout finished

---
+++ index.js
@@ -0,0 +1,32 @@
+/* sticky navbar */
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+
+window.onload = () => {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.remove('hidden');
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.add('hidden');
+    navbar.classList.remove("sticky");
+  }
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = () => toggleNavbar();
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+toggleNavbar = () => {
+  // console.log(window.pageYOffset);
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.remove('hidden');
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.add('hidden');
+    navbar.classList.remove("sticky");
+  }
+}
+	
+// ScrollReveal().reveal('section, article', { duration: 5000 }, { easing: 'cubic-bezier(.25,.6,.8,1)' });
